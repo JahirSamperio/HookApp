e8803c64d4da6fb8e4d01e6686388237539957cc
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-07-03 23:46:48 -0600

    Rutas y componentes de useContext

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom"
+import {HomePage} from './HomePage'
+import {AboutPage} from './AboutPage'
+import {LoginPage} from './LoginPage'
+
+export const MainApp = () => {
+    return (
+        <>
+            <h1>Main App</h1>
+            <hr/>
+
+            <Routes>
+                <Route path='/'     element={<HomePage />} />
+                <Route path='login' element={<LoginPage/>} />
+                <Route path='about' element={<AboutPage/>} />
+
+                <Route path='/*' element={<AboutPage/>} /> //Comodin para rutas que no existan y redirigirlas
+
+
+            </Routes>
+        </>
+    )
+}
